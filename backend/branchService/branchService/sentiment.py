f87070192b96eeee593c17b3585c7b37dd32aa73
yuande <miraclecome@gmail.com>
2018-10-14 00:58:53 +0800

    sentiment

---
+++ backend/branchService/branchService/sentiment.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Author: Yuande Liu <miraclecome (at) gmail.com>
+
+from aylienapiclient import textapi
+
+app_id = ""
+app_key = ""
+sentiment_client = textapi.Client(app_id, app_key)
+
--- backend/branchService/branchService/views.py
+++ backend/branchService/branchService/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from sentiment import sentiment_client
+
 import requests
 import json
 
 def extract_time(request):
     url = 'http://127.0.0.1:8000/parse'
+    text = request.GET.get('text')
     print(request.GET)
     data = {
         'locale': request.GET.get('locale', ['en_GB'])[0],
         'tz': request.GET.get('tz', ['Asia/Shanghai'])[0],
-        'text': request.GET.get('text')
+        'text': text
     }
     resp = requests.post(url, data=data)
     js = json.loads(resp.content[1:-1])
     result = js['value']['values'][0]['value']
+
+    s = sentiment_client.Sentiment({'text': text})
+    print(s)
 
     return HttpResponse(result)
