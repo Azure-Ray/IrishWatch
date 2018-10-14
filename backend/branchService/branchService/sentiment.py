f796fa386e821da936c73f5138cbab4b22dba0c2
yuande <miraclecome@gmail.com>
2018-10-14 20:41:00 +0800

    add

--- backend/branchService/branchService/sentiment.py
+++ backend/branchService/branchService/sentiment.py
@@ -12,4 +12,5 @@ def analysis_sentiment(text):
     s = sentiment_client.Sentiment({'text': text})
     #{'polarity': 'neutral', 'subjectivity': 'subjective', 'text': 'ten half of tomorrow morning', 'polarity_confidence': 0.6261608600616455, 'subjectivity_confidence': 0.9999999860809148}
 
+    # positive neutral
     return s['polarity']
